3c4376bf99a213d938c71d6224f017d7b902ef35
harisA1 <118927126+harisA1@users.noreply.github.com>
2023-01-21 20:25:17 +0200

    Delete demo directory

--- demo/src/main/java/com/example/demo/hello/TimeslotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.hello;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//Interface for the Timeslot to connect with the JPA Timeslot
-public interface TimeslotRepository extends JpaRepository<Timeslot, String> {
-}
