74fc3842a3aa66283bd2460f4d8bd52170670ccc
harisA1 <118927126+harisA1@users.noreply.github.com>
2023-01-30 12:09:05 +0200

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/hello/HelloConfiguration.java
@@ -0,0 +1,84 @@
+package com.example.demo.hello;
+
+import com.example.demo.objectclasses.*;
+import org.springframework.boot.*;
+import org.springframework.context.annotation.*;
+import org.springframework.beans.factory.annotation.*;
+
+@Configuration
+public class HelloConfiguration implements CommandLineRunner {
+
+    @Autowired
+    private HelloService hs;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Timeslot timeslot1 = new Timeslot("24/04/2023", "10:00");
+        Timeslot timeslot2 = new Timeslot("25/04/2023", "11:00");
+
+        Patient patient1 = new Patient("Haris", "Ampas", "23003");
+        Patient patient2 = new Patient("Christos", "Ioannou", "23017");
+        Patient patient3 = new Patient("Despoina", "Taskoudi", "23018");
+
+        Doctor doctor1 = new Doctor("123123","Dr.House");
+        Doctor doctor2 = new Doctor( "321321","Dr.Who");
+        Doctor doctor3 = new Doctor( "312312","Dr.Ampatzoglou");
+
+        VaccinationCenter vc1 = new VaccinationCenter("ΑΧΕΠΑ");
+        VaccinationCenter vc2 = new VaccinationCenter("ΓΝΣ ΓΕΝΝΗΜΑΤΑΣ");
+        VaccinationCenter vc3 = new VaccinationCenter("ΓΝΣ ΑΓΙΟΣ ΔΗΜΗΤΡΙΟΣ");
+        VaccinationCenter vc4 = new VaccinationCenter("ΙΠΠΟΚΡΑΤΕΙΟ");
+        VaccinationCenter vc5 = new VaccinationCenter("ΔΕΘ HELEXPO");
+        VaccinationCenter vc6 = new VaccinationCenter("ΠΑΜΑΚ");
+
+        hs.addPatient( patient1 );
+        hs.addPatient( patient2 );
+
+//        doctor1.addTimeslot(timeslot2);
+        hs.addDoctor(doctor1);
+        hs.addDoctor(doctor2);
+
+        //hs.addVaccinationCenter(vc1);
+        hs.addVaccinationCenter(vc2);
+        hs.addVaccinationCenter(vc3);
+        hs.addVaccinationCenter(vc4);
+        hs.addVaccinationCenter(vc5);
+        hs.addVaccinationCenter(vc6);
+
+        //hs.addTimeslot(timeslot1);
+        //hs.addTimeslot( timeslot2);
+
+        AvailableTimeslotInVc at1 = new AvailableTimeslotInVc(timeslot1, vc1, doctor1);
+        AvailableTimeslotInVc at2 = new AvailableTimeslotInVc(timeslot2, vc1, doctor1);
+
+        hs.addAvailableTimeslot(at1);
+        hs.addAvailableTimeslot(at2);
+        Appointment appointment1 = new Appointment(timeslot1,patient1);
+        Appointment appointment2 = new Appointment(timeslot2,patient2);
+        hs.addAppointment(appointment1);
+        hs.addAppointment(appointment2);
+        Vaccination vaccination1 = new Vaccination(timeslot1.getDate());
+        Vaccination vaccination2 = new Vaccination(timeslot2.getDate());
+        Vaccination vaccination3 = new Vaccination(timeslot2.getDate());
+        vaccination1.setPatient(patient1);
+        vaccination1.setDoctor(doctor1);
+        vaccination2.setPatient(patient2);
+        vaccination2.setDoctor(doctor2);
+        vaccination3.setPatient(patient3);
+        vaccination3.setDoctor(doctor3);
+        hs.addVaccination(vaccination1);
+        hs.addVaccination(vaccination2);
+        hs.addVaccination(vaccination3);
+
+//        AvailableTimeslotInVc at2 = new AvailableTimeslotInVc(timeslot2, vc2, doctor1);
+//        hs.addAvailableTimeslot(at2);
+//        Appointment appointment2 = new Appointment(timeslot2,patient2);
+//        hs.addAppointment(appointment2);
+
+
+
+
+
+    }
+}
---
+++ demo/src/main/java/com/example/demo/objectclasses/Doctor.java
@@ -0,0 +1,58 @@
+package com.example.demo.objectclasses;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "doctor")
+public class Doctor {
+    @Id
+    String damka;
+    String doctorname;
+
+    @OneToMany(mappedBy ="doctor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Timeslot> timeslots= new HashSet<>();
+
+//    @OneToMany
+//    @JoinTable(name="Vaccinations",
+//            joinColumns = @JoinColumn(name="doctor_amka"),
+//            inverseJoinColumns = @JoinColumn(name="patient_amka"))
+//    private List<Patient> patients;
+
+    //default constructor
+    public Doctor() {
+    }
+    //constructor
+    public Doctor(String doctorAmka,String doctorName){
+        this.damka=doctorAmka;
+        this.doctorname=doctorName;
+
+    }
+    //getter for doctor variables
+    public String getDamka() {
+        return damka;
+    }
+    public String getDoctorname() {
+        return doctorname;
+    }
+    //setter for doctor variables
+    public void setDamka(String damka) {this.damka = damka;}
+    public void setDoctorname(String doctorname) {this.doctorname = doctorname;}
+
+    //getters and setters for timeslots
+
+
+    public Set<Timeslot> getTimeslots() {return timeslots;}
+    public void setTimeslots(Set<Timeslot> timeslots) {this.timeslots = timeslots;}
+
+    //add timeslot
+    public void addTimeslot(Timeslot t) {timeslots.add(t);}
+
+
+
+}
