2e7cb3dba1cccf74b78b05fdae1dc3f234bfeb58
harisA1 <118927126+harisA1@users.noreply.github.com>
2023-01-30 12:08:11 +0200

    Delete demo directory

--- demo/src/main/java/com/example/demo/repositories/VaccinationCenterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.objectclasses.VaccinationCenter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VaccinationCenterRepository extends JpaRepository<VaccinationCenter, String> {
-}
